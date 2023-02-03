1e888fcd68b5131230e4e6b154460a38a8cb6b9c
Alice1319 <alice_lai1319@outlook.com>
2023-02-03 16:22:46 +0800

    Deprecate imagesBuilder when create builder.

--- plugins/huaweicloud/src/main/java/org/eclipse/xpanse/orchestrator/plugin/huaweicloud/BuilderFactory.java
+++ plugins/huaweicloud/src/main/java/org/eclipse/xpanse/orchestrator/plugin/huaweicloud/BuilderFactory.java
@@ -7,6 +7,7 @@
 package org.eclipse.xpanse.orchestrator.plugin.huaweicloud;
 
 import java.util.Optional;
+import lombok.extern.slf4j.Slf4j;
 import org.eclipse.xpanse.modules.ocl.loader.data.models.Ocl;
 import org.eclipse.xpanse.orchestrator.plugin.huaweicloud.builders.HuaweiEnvBuilder;
 import org.eclipse.xpanse.orchestrator.plugin.huaweicloud.builders.HuaweiImageBuilder;
@@ -15,9 +16,15 @@ import org.eclipse.xpanse.orchestrator.plugin.huaweicloud.builders.HuaweiResourc
 /**
  * Factory class to instantiate builder object.
  */
+@Slf4j
 public class BuilderFactory {
 
+    public static final String ENV_BUILDER = "env";
+
     public static final String BASIC_BUILDER = "basic";
+
+    public static final String BASIC_BUILDER_DEPRECATED = "basic_deprecated";
+
 
     /**
      * Factory method that instantiates complete builder object.
@@ -27,18 +34,19 @@ public class BuilderFactory {
      * @return AtomBuilder object.
      */
     public Optional<AtomBuilder> createBuilder(String builderType, Ocl ocl) {
+        if (builderType.equals(ENV_BUILDER)) {
+            return Optional.of(new HuaweiEnvBuilder(ocl));
+        }
         if (builderType.equals(BASIC_BUILDER)) {
-            HuaweiEnvBuilder envBuilder = new HuaweiEnvBuilder(ocl);
+            return Optional.of(new HuaweiResourceBuilder(ocl));
+        }
+        if (builderType.equals(BASIC_BUILDER_DEPRECATED)) {
             HuaweiImageBuilder imageBuilder = new HuaweiImageBuilder(ocl);
             HuaweiResourceBuilder resourceBuilder = new HuaweiResourceBuilder(ocl);
-            HuaweiEnvBuilder envBuilderTail = new HuaweiEnvBuilder(ocl);
-
-            imageBuilder.addSubBuilder(envBuilder);
             resourceBuilder.addSubBuilder(imageBuilder);
-            envBuilderTail.addSubBuilder(resourceBuilder);
-
-            return Optional.of(envBuilderTail);
+            return Optional.of(resourceBuilder);
         }
+        log.info("BuilderType:{} not supported.", builderType);
         return Optional.empty();
     }
 }
