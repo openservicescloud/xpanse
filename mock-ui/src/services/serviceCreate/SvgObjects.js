1d39401f5fe239c93bb212698c8b984c0a0c0b9d
kylin321370 <kylin321370@outlook.com>
2023-02-21 18:50:01 +0800

    update antui20230221

---
+++ mock-ui/src/services/serviceCreate/SvgObjects.js
@@ -0,0 +1,19 @@
+import ApacheKafka from './img/ApacheKafka.svg'
+import Mysql from './img/Mysql.svg'
+import Redis from './img/Redis.svg'
+import InfluxDB from './img/InfluxDB.svg'
+import OpenSearch from './img/OpenSearch.svg'
+import Grafana from './img/Grafana.svg'
+import Aws from './img/aws.png'
+import Google from './img/google.png'
+import Upcloud from './img/upcloud.png'
+
+export {ApacheKafka};
+export {Mysql};
+export {Redis};
+export {InfluxDB};
+export {OpenSearch};
+export {Grafana};
+export {Aws};
+export {Google};
+export {Upcloud};
