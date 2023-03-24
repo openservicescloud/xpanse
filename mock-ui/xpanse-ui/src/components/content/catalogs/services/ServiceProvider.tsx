e58218eeac87db5916904ba09854402ddc989eec
WangLiNaruto <wl-naruto@outlook.com>
2023-03-24 13:31:19 +0800

    update area and xpanse Api

--- mock-ui/xpanse-ui/src/components/content/catalogs/services/ServiceProvider.tsx
+++ mock-ui/xpanse-ui/src/components/content/catalogs/services/ServiceProvider.tsx
@@ -3,11 +3,12 @@
  * SPDX-FileCopyrightText: Huawei Inc.
  */
 
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Tabs } from 'antd';
 import ServiceDetail from './ServiceDetail';
-import { CategoryOclVo, OclDetailVo, ProviderOclVo, VersionOclVo } from '../../../../xpanse-api/generated';
+import { CategoryOclVo, OclDetailVo, ProviderOclVo, Region, VersionOclVo } from '../../../../xpanse-api/generated';
 import { Tab } from 'rc-tabs/lib/interface';
+import { Area } from '../../../utils/Area';
 
 let lastServiceName: string = '';
 
@@ -20,11 +21,24 @@ function ServiceProvider({
 }): JSX.Element {
     const [activeKey, setActiveKey] = useState<string>('');
     const [serviceDetails, setServiceDetails] = useState<OclDetailVo>(new OclDetailVo());
-    const [serviceRegions, setServiceRegions] = useState<string[]>([]);
+    const [serviceAreas, setServiceAreas] = useState<Area[]>([]);
 
     const detailMapper: Map<string, OclDetailVo> = new Map<string, OclDetailVo>();
-    const regionMapper: Map<string, string[]> = new Map<string, string[]>();
+    const areaMapper: Map<string, Area[]> = new Map<string, Area[]>();
     const [name, version] = serviceName.split('@');
+
+    function group(list: any[], key: string): Map<string, any[]> {
+        let map: Map<string, any[]> = new Map<string, any[]>();
+        list.map((val) => {
+            if (!map.has(val[key])) {
+                map.set(
+                    val[key],
+                    list.filter((data) => data[key] === val[key])
+                );
+            }
+        });
+        return map;
+    }
 
     const items: Tab[] = categoryOclData
         .filter((service: CategoryOclVo) => service.name === name)
@@ -40,7 +54,22 @@ function ServiceProvider({
                 }
                 const key = serviceName + '@' + cloudProvider.name;
                 detailMapper.set(key, cloudProvider.details[0]);
-                regionMapper.set(key, cloudProvider.regions || []);
+                const result: Map<string, Region[]> = group(cloudProvider.regions, 'area');
+                let areas: Area[] = [];
+
+                result.forEach((v, k) => {
+                    let regions: string[] = [];
+
+                    v.forEach((region) => {
+                        if (region.name != null) {
+                            regions.push(region.name);
+                        }
+                    });
+                    let area: Area = { name: k, regions: regions };
+                    areas.push(area);
+                });
+
+                areaMapper.set(key, areas);
                 const name = cloudProvider.name!.toString();
                 return {
                     label: name,
@@ -50,33 +79,34 @@ function ServiceProvider({
             });
         });
 
-    useEffect(() => {
-        if (items.length > 0 && lastServiceName !== serviceName) {
-            const key = serviceName + '@' + items[0].key;
-            const details = detailMapper.get(key);
-            if (details) {
-                setServiceDetails(details);
-            }
-            const regions = regionMapper.get(key);
-            if (regions) {
-                setServiceRegions(regions);
-            }
-            setActiveKey(items[0]!.key);
-        }
-        lastServiceName = serviceName;
-    }, [items]);
-
-    const onChange = (key: string) => {
-        setActiveKey(key);
-        const serviceKey = serviceName + '@' + key;
-        const regions = regionMapper.get(serviceKey);
+    function updateServiceDetails(serviceKey: string): void {
+        const areas = areaMapper.get(serviceKey);
         const details = detailMapper.get(serviceKey);
         if (details) {
             setServiceDetails(details);
         }
-        if (regions) {
-            setServiceRegions(regions);
+        if (areas) {
+            setServiceAreas(areas);
         }
+    }
+    useEffect(() => {
+        if (items.length > 0 && lastServiceName !== serviceName) {
+            updateServiceDetails(serviceName + '@' + items[0].key);
+            setActiveKey(items[0]!.key);
+        } else if (items.length > 0 && lastServiceName === serviceName) {
+            setActiveKey(items[0]!.key);
+        }
+        lastServiceName = serviceName;
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [serviceName]);
+
+    useEffect(() => {
+        updateServiceDetails(serviceName + '@' + activeKey);
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [activeKey]);
+
+    const onChange = (key: string) => {
+        setActiveKey(key);
     };
 
     return (
@@ -84,7 +114,7 @@ function ServiceProvider({
             {serviceName.length > 0 ? (
                 <>
                     <Tabs items={items} onChange={onChange} activeKey={activeKey} className={'ant-tabs-tab-btn'} />
-                    <ServiceDetail serviceDetails={serviceDetails} serviceRegions={serviceRegions} />
+                    <ServiceDetail serviceDetails={serviceDetails} serviceAreas={serviceAreas} />
                 </>
             ) : (
                 <></>
